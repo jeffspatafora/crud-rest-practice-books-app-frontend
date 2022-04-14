d9dcdea54132a6bbd3ebd2b57c29798d24730329
jeffspatafora <jeffspatafora@gmail.com>
2022-04-13 23:45:41 -0700

    adds index books action

--- src/views/BooksIndex.vue
+++ src/views/BooksIndex.vue
@@ -1,18 +1,36 @@
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "All Books",
+      books: []
     };
   },
-  created: function () { },
-  methods: {},
+  created: function () {
+    this.indexBooks();
+  },
+  methods: {
+    indexBooks: function () {
+      console.log("index books");
+      axios.get("/books").then(response => {
+        console.log(response.data);
+        this.books = response.data;
+      });
+    },
+  },
 };
 </script>
 
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="book in books" v-bind:key="book.id">
+      <h2>{{ book.title }}</h2>
+      <p>{{ book.author }} <br /> {{ book.year }}</p>
+      <img v-bind:src="book.image">
+    </div>
+    <button v-on:click="indexBooks()">all books</button>
   </div>
 </template>
 
